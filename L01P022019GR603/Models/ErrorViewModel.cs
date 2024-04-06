07857cdec91dff309f50728f37f73e037aa48b41
Estudiante <Estudiante@PC01B45>
2024-04-06 13:10:01 -0600

    Agregar archivos de proyecto.

---
+++ L01P022019GR603/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P022019GR603.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ L01P022019GR603/Models/notasDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace L01P022019GR603.Models
+{
+    public class notasDbContext: DbContext
+    {
+        public notasDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
